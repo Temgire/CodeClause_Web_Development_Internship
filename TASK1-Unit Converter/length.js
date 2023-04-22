599d00c6ef1115dbeb99fe3c56ee9a84a1379373
Temgire Dinesh <95518302+Temgire@users.noreply.github.com>
2023-04-22 10:24:32 +0530

    Add files via upload

---
+++ TASK1-Unit Converter/length.js
@@ -0,0 +1,101 @@
+var input = document.getElementById('input');
+var result = document.getElementById('result');
+var inputType = document.getElementById('inputType');
+var resultType = document.getElementById('resultType');
+var option_from,option_to;
+
+input.addEventListener("keyup",myResult);
+inputType.addEventListener("change",myResult);
+resultType.addEventListener("change",myResult);
+
+
+option_from = inputType.value;
+option_to   = resultType.value;
+
+
+function myResult(){
+
+	option_from = inputType.value;
+	option_to = resultType.value;
+
+
+	if(option_from === "meter" && option_to==="kilometer")
+	{
+		result.value = Number(input.value) * 0.001;
+	}
+	else if(option_from === "meter" && option_to==="Centimeter")
+	{
+		result.value = Number(input.value) * 100;
+
+	}
+	else if(option_from === "meter" && option_to==="meter")
+	{
+		result.value = input.value
+	}
+	else if(option_from === "meter" && option_to==="MiliMeater")
+	{
+		result.value = Number(input.value) * 1000;
+
+	}
+
+
+
+	if(option_from === "kilometer" && option_to==="meter")
+	{
+		result.value = Number(input.value) * 1000;
+	}
+	else if(option_from === "kilometer" && option_to==="Centimeter")
+	{
+		result.value = Number(input.value) * 100000;
+	}
+	else if(option_from === "kilometer" && option_to==="kilometer")
+	{
+		result.value = input.value
+	}
+	else if(option_from === "kilometer" && option_to==="MiliMeater")
+	{
+		result.value = Number(input.value) * 10000000;
+
+	}
+
+
+
+	if(option_from === "Centimeter" && option_to==="kilometer")
+	{
+		result.value = Number(input.value) * 0.00001;
+	}
+	else if(option_from === "Centimeter" && option_to==="meter")
+	{
+		result.value = Number(input.value) * (0.01);
+	}
+	else if(option_from === "Centimeter" && option_to==="Centimeter")
+	{
+		result.value = input.value
+	}
+	else if(option_from === "Centimeter" && option_to==="MiliMeater")
+	{
+		result.value = Number(input.value) * 10;
+
+	}
+
+
+	
+	if(option_from === "MiliMeater" && option_to==="kilometer")
+	{
+		result.value = Number(input.value) *0.000001;
+	}
+	else if(option_from === "MiliMeater" && option_to==="meter")
+	{
+		result.value = Number(input.value) * (0.001);
+	}
+	else if(option_from === "MiliMeater" && option_to==="Centimeter")
+	{
+		result.value = Number(input.value) *0.1;
+	}
+	else if(option_from === "MiliMeater" && option_to==="MiliMeater")
+	{
+		result.value = input.value
+
+	}
+
+}
